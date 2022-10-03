e3618f731dad5c3af21427ccf51673958aea4fc0
Fe-56 <limfuoen1999@gmail.com>
2022-10-03 22:52:17 +0800

    Update

--- src/Scatterplot.js
+++ src/Scatterplot.js
@@ -17,17 +17,26 @@ const Scatterplot = (props) => {
             for (let i = 0; i < rawDataset.length; i++){
                 const year = rawDataset[i].Year;
                 const time = rawDataset[i].Time;
+                const doping = rawDataset[i].Doping === "" ? false : true;
+                const name = rawDataset[i].Name;
+                const nationality = rawDataset[i].Nationality;
+                const dopingDescription = rawDataset[i].Doping;
                 years.push(year);
                 times.push(time);
-                dataset.push([year, time]);
+                dataset.push({
+                    year: year,
+                    time: time,
+                    doping: doping,
+                    name: name,
+                    nationality: nationality,
+                    dopingDescription: dopingDescription
+                });
             }
 
             let timeMin = d3.min(times);
             let timeMax = d3.max(times);
             timeMin = mmssToDate(timeMin);
             timeMax = mmssToDate(timeMax);
-
-            console.log(dataset)
 
             const xScale = d3.scaleLinear()
                             .domain([d3.min(years) - 1, d3.max(years) + 1])
@@ -41,38 +50,111 @@ const Scatterplot = (props) => {
                             .tickFormat(d3.timeFormat("%M:%S"))
                             .ticks(d3.timeSecond.every(15));
 
-            var svg = d3.select("#holder")
-                        .append("svg")
-                        .attr("width", width + 100)
-                        .attr("height", height)
-                        .append("g")
-                        .call(xAxis)
-                        .attr("id", "x-axis")
-                        .attr("transform", `translate(60, ${height - 40})`)
-                        .append("g")
-                        .call(yAxis)
-                        .attr("id", "y-axis")
-                        .attr("transform", `translate(0, ${-height })`);
+            d3.select("#holder")
+                .append("svg")
+                .attr("width", width + 100)
+                .attr("height", height)
+                .append("g")
+                .call(xAxis)
+                .attr("id", "x-axis")
+                .attr("transform", `translate(60, ${height - 40})`)
+                .attr("color", "white");
 
-            svg.selectAll("circle")
+            d3.select("svg")
+                .append("g")
+                .call(yAxis)
+                .attr("id", "y-axis")
+                .attr("transform", `translate(60, -40)`)
+                .attr("color", "white");
+
+            const dopingColor = "#FF2975";
+            const noDopingColor = "#FFD319";
+
+            d3.select("svg")
+                .selectAll("circle")
                 .data(dataset)
                 .enter()
                 .append("circle")
                 .attr("class", "dot")
                 .attr("cx", (datapoint) => {
-                    return xScale(datapoint[0]);
+                    return xScale(datapoint.year) + 60;
                 })
                 .attr("cy", (datapoint) => {
-                    return yScale(mmssToDate(datapoint[1]));
+                    return yScale(mmssToDate(datapoint.time)) - 40;
                 })
                 .attr("r", 8)
                 .attr("data-xvalue", (datapoint) => {
-                    return datapoint[0];
+                    return datapoint.year;
                 })
                 .attr("data-yvalue", (datapoint) => {
-                    return mmssToDate(datapoint[1]);
+                    return mmssToDate(datapoint.time);
                 })
-                .attr("fill", "black")
+                .attr("fill", (datapoint) => {
+                    if (datapoint.doping){ // with doping allegation
+                        return dopingColor;
+                    }
+
+                    else{ // no doping allegation
+                        return noDopingColor;
+                    }
+                })
+                .on("mouseover", (event, item) => {
+                    let tooltipTextColor;
+                    let tooltipText =  `${item.name}, ${item.nationality}<br />Year: ${item.year}<br />Time: ${item.time}`;
+
+                    if (item.doping){ // with doping allegation
+                        tooltipTextColor = dopingColor;
+                        tooltipText += `<br />${item.dopingDescription}`;
+                    }
+
+                    else{ // no doping allegation
+                        tooltipTextColor = noDopingColor;
+                    }
+
+                    d3.select(event.currentTarget).attr("r", 15);
+                    const tooltip = document.getElementById("tooltip");
+                    tooltip.style.visibility = "visible";
+                    tooltip.style.color = tooltipTextColor;
+                    tooltip.innerHTML = tooltipText;
+                    tooltip.setAttribute("data-year", d3.select(event.currentTarget).attr("data-xvalue"));
+                })
+                .on("mouseout", (event) => {
+                    document.getElementById("tooltip").style.visibility = "hidden";
+                    d3.select(event.currentTarget).attr("r", 8);
+                });
+
+            const legendX = 1000
+            const legendY = 100
+
+            d3.select("svg")
+                .append("g")
+                .attr("id", "legend")
+                .append("circle")
+                .attr("cx", legendX)
+                .attr("cy", legendY)
+                .attr("r", 15)
+                .attr("fill", "#FF2975"); // for those with doping allegations
+
+            d3.select("#legend")
+                .append("text")
+                .attr("class", "legend-label")
+                .attr("x", legendX + 30)
+                .attr("y", legendY + 5)
+                .text("Rider with doping allegation");
+
+            d3.select("#legend")
+                .append("circle")
+                .attr("cx", legendX)
+                .attr("cy", legendY + 40)
+                .attr("r", 15)
+                .attr("fill", "#FFD319"); // for those without doping allegations
+
+            d3.select("#legend")
+            .append("text")
+            .attr("class", "legend-label")
+            .attr("x", legendX + 30)
+            .attr("y", legendY + 45)
+            .text("Rider without doping allegation")
         });
     }, []);
 
